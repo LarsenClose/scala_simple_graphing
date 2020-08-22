9da605ac7fa45b59aeb497a285bb07698c952a96
Larsen Close <LarsenClose@protonmail.com>
2020-08-21 19:02:22 -0600

    working

--- src/main/scala/PLGraph.scala
+++ src/main/scala/PLGraph.scala
@@ -1,6 +1,7 @@
 package src
 
 import org.graphstream.graph.implementations.MultiGraph
+import org.graphstream.graph.{Edge, Node}
 
 object PLGraph {
 
@@ -11,11 +12,24 @@ object PLGraph {
   def main(args: Array[String]): Unit = {
 
     // create the graph
-    val graph = new MultiGraph("PL")
+    val graph = new MultiGraph("PL", false, true)
     graph.addAttribute("ui.stylesheet", "url('file://" + USER_DIR + "/" + STYLE + "')")
     graph.addAttribute("ui.antialias")
 
     // TODO: parse the PL_CSV_FILE to create a directed graph of PLs
+    val sourceData = io.Source.fromFile(PL_CSV_FILE)
+    for (line <- sourceData.getLines) {
+      var cols = line.split(",").map(_.trim)
+
+      var node: Node = graph.addNode(cols(0))
+      node.addAttribute("ui.label", cols(0))
+      node = graph.addNode(cols(1))
+      node.addAttribute("ui.label", cols(1))
+
+      var edge: Edge = graph.addEdge(cols(0) + cols(1), cols(0), cols(1), true)
+      graph.addEdge[Edge](cols(0) + cols(1), cols(0), cols(1), true)
+    }
+    sourceData.close
 
 
     // display the graph
